920f349e9d843fc2755b92c13b3a0909d2518e9a
Victoreze02 <ezemokwev@gmail.com>
2025-02-02 14:58:53 +0100

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Header from "../../my-portfolio/src/components/header";
-import Hero from "../../my-portfolio/src/components/hero";
+import Header from "./components/header";
+import Hero from "./components/hero";
 // import About from "../../my-portfolio/src/components/";
-import Projects from "../../my-portfolio/src/components/projects";
-import Footer from "../../my-portfolio/src/components/footer";
+import Projects from "./components/projects";
+import Footer from "./components/footer";
 
 
 function App() {
